ba251506cb8702662ff0490b35d7b02f0959001a
alaminmagaga <alaminmusamagaga@gmail.com>
2023-06-06 11:40:48 +0100

    update

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-    latitude = 11.987649978084008
-    longitude = 8.419178940425969
-    
+    latitude = 11.977501252244648
+    longitude = 8.420809723501117
+   
     context = {
         'latitude': latitude,
         'longitude': longitude
